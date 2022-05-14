dee3e9eb2853e98ed7717bf49e10ae15d44e7ae6
IrvingCM123 <98989957+IrvingCM123@users.noreply.github.com>
2022-05-14 17:41:37 -0500

    prueba

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const morgan = require("morgan");
+const path = require("path");
+const app = express();
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+// settings
+app.set("port", process.env.PORT || 3000);
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+
+// middlewares
+app.use(morgan("dev"));
+
+// routes
+app.use(require("./routes"));
+
+// static files
+app.use(express.static(path.join(__dirname, "public")));
+
+// listening the Server
+app.listen(app.get("port"), () => {
+  console.log("Server on port", app.get("port"));
+});
