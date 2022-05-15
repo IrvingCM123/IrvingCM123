34c5cfd9ccdbd45a08bd6391e43b0f1e41e53730
IrvingCM123 <98989957+IrvingCM123@users.noreply.github.com>
2022-05-14 19:56:39 -0500

    prueba

--- src/index.js
+++ src/index.js
@@ -10,9 +10,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // settings
 app.set("port", process.env.PORT || 3000);
 app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
 app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
+app.set("view engine", "ejs");
 
 
 // middlewares
